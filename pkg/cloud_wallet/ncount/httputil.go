76c13426c2675e7de0707b7d857609d2e4169a78
steven <steven>
2023-03-23 21:12:43 +0800

    feature(pkg): 添加验证、签名、解密方法

--- pkg/cloud_wallet/ncount/httputil.go
+++ pkg/cloud_wallet/ncount/httputil.go
@@ -2,6 +2,7 @@ package ncount
 
 import (
 	"bytes"
+	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha256"
@@ -48,4 +49,69 @@ func Encrpt(message []byte, publicKeyString string) ([]byte, error) {
 	}
 	encoded := base64.StdEncoding.EncodeToString(ciphertext)
 	return []byte(encoded), nil
+}
+
+func Decrypt(message []byte, privateKeyString string) ([]byte, error) {
+	if privateKeyString == "" {
+		return nil, fmt.Errorf("privateKeyString is empty")
+	}
+	privateKeyBlock, _ := pem.Decode([]byte(privateKeyString))
+	if privateKeyBlock == nil {
+		return nil, fmt.Errorf("privateKeyString is invalid")
+	}
+	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	decoded, err := base64.StdEncoding.DecodeString(string(message))
+	if err != nil {
+		return nil, err
+	}
+	plaintext, err := rsa.DecryptOAEP(
+		sha256.New(),
+		rand.Reader,
+		privateKey,
+		decoded,
+		nil,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return plaintext, nil
+}
+
+func Sign(message []byte, privateKeyString string) ([]byte, error) {
+	if privateKeyString == "" {
+		return nil, fmt.Errorf("privateKeyString is empty")
+	}
+	privateKeyBlock, _ := pem.Decode([]byte(privateKeyString))
+	if privateKeyBlock == nil {
+		return nil, fmt.Errorf("privateKeyString is invalid")
+	}
+	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	hashed := sha256.Sum256(message)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed[:])
+	if err != nil {
+		return nil, err
+	}
+	return signature, nil
+}
+
+func Verify(message []byte, signature []byte, publicKeyString string) error {
+	if publicKeyString == "" {
+		return fmt.Errorf("publicKeyString is empty")
+	}
+	publicKeyBlock, _ := pem.Decode([]byte(publicKeyString))
+	if publicKeyBlock == nil {
+		return fmt.Errorf("publicKeyString is invalid")
+	}
+	publicKey, err := x509.ParsePKIXPublicKey(publicKeyBlock.Bytes)
+	if err != nil {
+		return err
+	}
+	hashed := sha256.Sum256(message)
+	return rsa.VerifyPKCS1v15(publicKey.(*rsa.PublicKey), crypto.SHA256, hashed[:], signature)
 }
