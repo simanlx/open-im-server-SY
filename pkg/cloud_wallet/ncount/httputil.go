8be9057a48697639cc619a534f2a394fc9e28eaa
steven <steven>
2023-03-24 19:58:10 +0800

    完成新生支付联通测试

--- pkg/cloud_wallet/ncount/httputil.go
+++ pkg/cloud_wallet/ncount/httputil.go
@@ -5,10 +5,12 @@ import (
 	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/pem"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -23,83 +25,45 @@ func httpPost(url string, body []byte) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
-// 1. 用新账通平台公钥对字符串进行非对称加密；
+// 1. 用新账通平台公钥对json字符串进行非对称加密；
 // 2. 对加密后的二进制转 Base64 编码
-func Encrpt(message []byte, publicKeyString string) ([]byte, error) {
-	if publicKeyString == "" {
-		return nil, fmt.Errorf("publicKeyString is empty")
-	}
-	publicKeyBlock, _ := pem.Decode([]byte(publicKeyString))
+func Encrpt(message []byte, key string) ([]byte, error) {
+	publicKeyBlock, _ := pem.Decode([]byte(key))
 	if publicKeyBlock == nil {
-		return nil, fmt.Errorf("publicKeyString is invalid")
+		return nil, fmt.Errorf("Key is invalid")
 	}
 	publicKey, err := x509.ParsePKIXPublicKey(publicKeyBlock.Bytes)
 	if err != nil {
-		return nil, err
+		return nil, errors.New("Encrpt ParsePKIXPublicKey :" + err.Error())
 	}
-	ciphertext, err := rsa.EncryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		publicKey.(*rsa.PublicKey),
-		message,
-		nil,
-	)
+	cipherByte, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey.(*rsa.PublicKey), message)
 	if err != nil {
 		return nil, err
 	}
-	encoded := base64.StdEncoding.EncodeToString(ciphertext)
+	encoded := base64.StdEncoding.EncodeToString(cipherByte)
 	return []byte(encoded), nil
 }
 
-func Decrypt(message []byte, privateKeyString string) ([]byte, error) {
-	if privateKeyString == "" {
-		return nil, fmt.Errorf("privateKeyString is empty")
-	}
-	privateKeyBlock, _ := pem.Decode([]byte(privateKeyString))
-	if privateKeyBlock == nil {
-		return nil, fmt.Errorf("privateKeyString is invalid")
-	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
+// 完成签名认证 ，通过单元测试
+func Sign(message []byte, privateKeyString string) (string, error) {
+	privateKeyBytes, err := base64.StdEncoding.DecodeString(privateKeyString)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	decoded, err := base64.StdEncoding.DecodeString(string(message))
+	// 将私钥解析为 *rsa.PrivateKey 对象
+	privateKey, err := x509.ParsePKCS8PrivateKey(privateKeyBytes)
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	plaintext, err := rsa.DecryptOAEP(
-		sha256.New(),
-		rand.Reader,
-		privateKey,
-		decoded,
-		nil,
-	)
+	hashed := sha1.Sum(message)
+	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey.(*rsa.PrivateKey), crypto.SHA1, hashed[:])
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	return plaintext, nil
+	return base64.StdEncoding.EncodeToString(signature), nil
 }
 
-func Sign(message []byte, privateKeyString string) ([]byte, error) {
-	if privateKeyString == "" {
-		return nil, fmt.Errorf("privateKeyString is empty")
-	}
-	privateKeyBlock, _ := pem.Decode([]byte(privateKeyString))
-	if privateKeyBlock == nil {
-		return nil, fmt.Errorf("privateKeyString is invalid")
-	}
-	privateKey, err := x509.ParsePKCS1PrivateKey(privateKeyBlock.Bytes)
-	if err != nil {
-		return nil, err
-	}
-	hashed := sha256.Sum256(message)
-	signature, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed[:])
-	if err != nil {
-		return nil, err
-	}
-	return signature, nil
-}
-
+// 使用公钥，进行验签
 func Verify(message []byte, signature []byte, publicKeyString string) error {
 	if publicKeyString == "" {
 		return fmt.Errorf("publicKeyString is empty")
